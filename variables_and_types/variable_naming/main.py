bf033abf616ea3676c9f74fbc11516a5c20a7241
Jago56 <>
2026-02-25 23:31:06 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,12 @@
+item_name = "Bread"
+item_price = 4.52
+items_in_stock = 230
+#float_item_price = item_price
+#int_num_positive = int(float_item_price)
+
+
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
